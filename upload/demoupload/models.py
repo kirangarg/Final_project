58d0ae2d297147afb75748978e7cb7c4fa511278
kiran <kirangarg700@gmail.com>
2017-07-24 22:08:27 +0530

    django signup created

--- upload/demoupload/models.py
+++ upload/demoupload/models.py
@@ -3,11 +3,13 @@ from __future__ import unicode_literals
 from django.db import models
 # Create your models here.
 
-class User(models.Model):
-    name = models.CharField(max_length=255)
-    age = models.IntegerField(default=0)
-    phone = models.CharField(max_length=255)
-    verified_phone =  models.BooleanField(default=False)
-    creation_date = models.DateTimeField(auto_now_add=True)
+class UserModel(models.Model):
+    email = models.EmailField()
+    name = models.CharField(max_length=120)
+    username = models.CharField(max_length=120)
+    password = models.CharField(max_length=40)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
 
 
